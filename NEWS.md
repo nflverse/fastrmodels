affd39d194a4728220ce5d434f5c65e11767bc46
Tan <tan@tanho.ca>
2025-12-06 14:58:29 -0500

    re-save xgboost models as internal data

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,7 @@
+# fastrmodels 2.1.0
+
+* The ~~cake~~ _promise of future xgboost compatibility_ is a lie. Back to internal rds files for now.
+
 # fastrmodels 2.0.0
 
 * All xgboost models have been saved as raw vectors in order to be compatible with all future xgboost versions.
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,12 +1,3 @@
-# This is how the models in this package were stored initially to save runtime
-# however please see the file MODELS.R for how they were created
-ep_model <- fastrmodels::ep_model
-wp_model <- fastrmodels::wp_model
-wp_model_spread <- fastrmodels::wp_model_spread
-fg_model <- fastrmodels::fg_model
-cp_model <- fastrmodels::cp_model
-xyac_model <- fastrmodels::xyac_model
-xpass_model <- fastrmodels::xpass_model
 
 # xgboost >= 1.6.0 warned the user because of old serialization formats.
 # So we save the models in the suggested serialized json format, read them
@@ -20,23 +11,23 @@ xpass_model <- fastrmodels::xpass_model
 # UPDATE 2025-05-12: re-exporting under each version doesn't make any sense.
 #                    Trying new approach by saving as raw vectors.
 #                    Requires update of nflfastR
-xgboost::xgb.save(ep_model, "ep_model.ubj")
-xgboost::xgb.save(wp_model, "wp_model.ubj")
-xgboost::xgb.save(wp_model_spread, "wp_model_spread.ubj")
-xgboost::xgb.save(cp_model, "cp_model.ubj")
-xgboost::xgb.save(xyac_model, "xyac_model.ubj")
-xgboost::xgb.save(xpass_model, "xpass_model.ubj")
+# UPDATE 2025-12-06: Above didn't work with release of xgboost 3.0.0 on CRAN,
+#                    newest xgboost docs suggest that saveRDS will magically
+#                    work again. I think in the 2026 offseason we will need to
+#                    adapt nflfastR to cache the ubj files from github at predict
+#                    time (and deprecate this package altogether).  - Tan
 
-# these are raw model vectors now. xgboost constructs the models of them
-# this will be compatible with future versions
-# MODELS NOW LIVE IN https://github.com/nflverse/fastrmodels/releases/tag/model_archive
-ep_model <- xgboost::xgb.load("data-raw/ep_model.ubj") |> xgboost::xgb.Booster.complete() |> xgboost::xgb.save.raw()
-wp_model <- xgboost::xgb.load("data-raw/wp_model.ubj") |> xgboost::xgb.Booster.complete() |> xgboost::xgb.save.raw()
-wp_model_spread <- xgboost::xgb.load("data-raw/wp_model_spread.ubj") |> xgboost::xgb.Booster.complete() |> xgboost::xgb.save.raw()
+# UBJ MODELS LIVE IN https://github.com/nflverse/fastrmodels/releases/tag/model_archive
+piggyback::pb_download(tag = "model_archive", dest = "data-raw")
+usethis::use_git_ignore("data-raw/*.ubj")
+
+ep_model <- xgboost::xgb.load("data-raw/ep_model.ubj")
+wp_model <- xgboost::xgb.load("data-raw/wp_model.ubj")
+wp_model_spread <- xgboost::xgb.load("data-raw/wp_model_spread.ubj")
 fg_model <- fastrmodels::fg_model
-cp_model <- xgboost::xgb.load("data-raw/cp_model.ubj") |> xgboost::xgb.Booster.complete() |> xgboost::xgb.save.raw()
-xyac_model <- xgboost::xgb.load("data-raw/xyac_model.ubj") |> xgboost::xgb.Booster.complete() |> xgboost::xgb.save.raw()
-xpass_model <- xgboost::xgb.load("data-raw/xpass_model.ubj") |> xgboost::xgb.Booster.complete() |> xgboost::xgb.save.raw()
+cp_model <- xgboost::xgb.load("data-raw/cp_model.ubj")
+xyac_model <- xgboost::xgb.load("data-raw/xyac_model.ubj")
+xpass_model <- xgboost::xgb.load("data-raw/xpass_model.ubj")
 
 # save models to use in package
 usethis::use_data(
